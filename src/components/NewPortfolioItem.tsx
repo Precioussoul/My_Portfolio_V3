2dba531946498b1a642b42ccd4b7cb8c81f8d447
Precioussoul <habsoff@gmail.com>
2023-12-04 21:40:19 +0100

    feat: working on new updates

--- src/components/NewPortfolioItem.tsx
+++ src/components/NewPortfolioItem.tsx
@@ -1,6 +1,21 @@
 import Image from "next/image"
+import Link from "next/link"
 import React, {useEffect} from "react"
 import Slider from "react-slick"
+import {FiExternalLink} from "react-icons/fi"
+import {FaBuildingLock, FaGithub} from "react-icons/fa6"
+
+type NewPortFolioItem = {
+  isPrivateRepo?: boolean
+  images?: string[]
+  isFeatured?: boolean
+  isDevMode?: boolean
+  projectTitle?: string
+  projectDescription?: string
+  projectLiveUrl?: string
+  projectGitHubUrl?: string
+  techStacks?: string
+}
 
 const NewPortfolioItem = () => {
   const settings = {
@@ -16,7 +31,7 @@ const NewPortfolioItem = () => {
 
   return (
     <div className='portfolio-new__item'>
-      <div className='relative w-[700px] h-full'>
+      <div className='relative 2xl:w-[700px] w-[550px] h-full hidden md:block order-2 md:order-1'>
         <Slider {...settings}>
           <div className='portfolio-new__img-item'>
             <div className='portfolio-new__item-img-box'>
@@ -56,7 +71,7 @@ const NewPortfolioItem = () => {
           </div>
         </Slider>
       </div>
-      <div className='portfolio-new__summary'>
+      <div className='portfolio-new__summary order-1 md:order-1'>
         <div className='portfolio-new__heading inline-flex flex-col justify-end items-end w-full p-4'>
           <span className='portfolio-new__heading-subtitle font-semibold  '>
             Featured Project
@@ -74,7 +89,7 @@ const NewPortfolioItem = () => {
         </div>
 
         <div className='inline-flex flex-col justify-end items-end w-full p-4'>
-          <div className='flex gap-4 items-center'>
+          <div className='flex gap-4 items-center flex-wrap'>
             {"React, Redux, Chakra UI, Typescript, Internal APIs,"
               .split(",")
               .map((stack, idx) => (
@@ -82,6 +97,16 @@ const NewPortfolioItem = () => {
                   {stack}
                 </p>
               ))}
+          </div>
+          <div className='flex gap-3'>
+            <FaBuildingLock className='project__link-icon' size={40} />
+
+            <Link href={""} target={"_blank"}>
+              <FaGithub className='project__link-icon' size={40} />
+            </Link>
+            <Link href={""} target={"_blank"}>
+              <FiExternalLink className='project__link-icon' size={40} />
+            </Link>
           </div>
         </div>
       </div>
