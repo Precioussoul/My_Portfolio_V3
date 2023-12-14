ed340b335e789d815f5c98bc83791860686b69ff
Precioussoul <habsoff@gmail.com>
2023-12-14 12:50:27 +0100

    feat: update favicon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,4 +1,4 @@
-import Script from 'next/script'
+import Script from "next/script"
 
 export default function Head() {
   return (
@@ -9,7 +9,7 @@ export default function Head() {
         name='description'
         content='A motivated, detail-oriented and committed software engineer with over 3 years of experience in gathering requirements, designing, implementing, testing, deploying and delivering pixel-perfect frontends, complex backends, mobile apps and cloud solutions using varieties of programming languages, frameworks, technologies and cloud vendors services as a solo engineer and as part of an engineering team.'
       />
-      <link rel='icon' href='assets/images/logo/habsof_Portfolio-favicon.png' />
+      <link rel='icon' href='/assets/images/logo/logo-habsof-ai.png' />
 
       <Script src='assets/vendor/fontawesome/js/all.min.js'></Script>
     </>
